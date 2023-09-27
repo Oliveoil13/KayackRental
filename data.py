e376083627d3be757d2bd4d9609cf652297746c2
Oliveoil13 <olivequinn13@gmail.com>
2023-09-26 21:59:06 -0600

    starting data base

---
+++ data.py
@@ -0,0 +1,28 @@
+
+
+Rollerskates = [
+    ['Moxie',
+      ['Beach Bunnies',['Size 4',30],['Size 5', 32],['Size 6', 35],],
+      ['Lollys',['Size 4',30],['Size 5', 32],['Size 6', 35],],
+     ],
+    ['Impalas',
+     ['Standard',['Size 4',30],['Size 5', 32],['Size 6', 35],]
+     ],
+    ['Moonlight',
+     ['Standard',['Size 4',30],['Size 5', 32],['Size 6', 35],]
+     ],
+    ]
+
+GrindBlocks = [
+    ['CIB',
+     ['Small', 40],['Medium',50],['Large',60],
+     ],
+    ['Wild Bone',
+     ['Small', 40],['Medium',50],['Large',60],
+     ],
+
+]
+#for x in Rollerskates:
+    #print(x)
+print(Rollerskates[0][1:])
+
