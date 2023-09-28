d5632c8954f37d2ab8152314b43607f8441a740f
Oliveoil13 <olivequinn13@gmail.com>
2023-09-27 23:34:05 -0600

    make drop downs

--- data.py
+++ data.py
@@ -1,28 +1,53 @@
 
-
 Rollerskates = [
     ['Moxie',
-      ['Beach Bunnies',['Size 4',30],['Size 5', 32],['Size 6', 35],],
-      ['Lollys',['Size 4',30],['Size 5', 32],['Size 6', 35],],
-     ],
+    ['Beach Bunnies',
+    ['Size 4',30],
+    ['Size 5', 32],
+    ['Size 6', 35],
+    ],
+    ['Lollys',
+    ['Size 4',30],
+    ['Size 5', 32],
+    ['Size 6', 35],
+    ],
+    ],
     ['Impalas',
-     ['Standard',['Size 4',30],['Size 5', 32],['Size 6', 35],]
-     ],
+    ['Standard',
+    ['Size 4',30],
+    ['Size 5', 32],
+    ['Size 6', 35],
+    ]
+    ],
     ['Moonlight',
-     ['Standard',['Size 4',30],['Size 5', 32],['Size 6', 35],]
-     ],
+    ['Standard',
+    ['Size 4',30],
+    ['Size 5', 32],
+    ['Size 6', 35],
+    ]
+    ],
     ]
 
 GrindBlocks = [
     ['CIB',
-     ['Small', 40],['Medium',50],['Large',60],
-     ],
+    ['Small', 40],
+    ['Medium',50],
+    ['Large',60],
+    ],
     ['Wild Bone',
-     ['Small', 40],['Medium',50],['Large',60],
-     ],
+    ['Small', 40],
+    ['Medium',50],
+    ['Large',60],
+    ],
 
 ]
+
+
+
 #for x in Rollerskates:
     #print(x)
-print(Rollerskates[0][1:])
+#print(Rollerskates[0][1:])
+
+for brand in Rollerskates:
+    print(brand[0])
 
