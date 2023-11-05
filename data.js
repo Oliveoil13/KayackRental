dd75f7868af8b5e92ba37a681ebe456322634b08
Oliveoil13 <olivequinn13@gmail.com>
2023-11-05 03:12:06 -0700

    it works i just don't know how to display in html

--- data.js
+++ data.js
@@ -43,13 +43,24 @@ const GrindBlocks = [
 
 ]
 
+//I need to make four functions here
 
+// first function, it needs to make a dynamic dropdown takes the first items in the idex to make the items in the dropdown
+//and then sets a varible to the idex of the one selected
 
-/*for x in Rollerskates:
-    print(x)
-print(Rollerskates[0][1:])
+let firstDropdown = "";
+for (let i = 0; i < Rollerskates[0].length; i++) {
+    firstDropdown += "<option value=\"" + "\">" + Rollerskates[i][0] + "</option>";
+}
 
-for brand in Rollerskates:
-    print(brand[0])
+console.log(firstDropdown);
+//second one displays the next set of items within the one that was choosen and does the same thing to the next item
 
-*/
+//third one displays teh size options depending on the previous one and then save that value instead of the idex
+
+//4th one is a drop down that uses the first order of indexes to display in the drop downs 
+
+//5th function when a button is clicked if not everything is selected correctly then it tells then to select those, then takes
+//vaule from the size that they chose and compares it to each of the sizes of the grindblock and determines which one fits the best 
+// and saves the name o that size as a variable and displays that at the end
+
