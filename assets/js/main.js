ca9ef278dd9c70999dbea79701334db44faf1af9
Olive Quinn <126294559+Oliveoil13@users.noreply.github.com>
2025-03-09 01:34:11 -0700

    better favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 /*
-	Easy Haul KayakD
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Easy Haul Kayak
 */
 
 var settings = {
