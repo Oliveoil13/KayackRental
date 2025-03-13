4daabb49d24c781215167b48a63cec6c20a00e17
Olive Quinn <126294559+Oliveoil13@users.noreply.github.com>
2025-03-13 00:15:05 -0600

    finishing touches

--- sendEmail.js
+++
@@ -1,9 +0,0 @@
-function sendMeAnEmail(){
-  let parms = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value,
-  } 
-  emailjs.send("service_tcfwpic","template_x2arf3e",parms).then(alert("Email Sent!!"))
-}
